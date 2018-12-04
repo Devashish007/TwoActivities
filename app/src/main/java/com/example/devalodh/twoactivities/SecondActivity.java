d04e507ec1488de2d097e2e900e393ed68863f58
CrimsonCurse <devashishlodh@gmail.com>
2018-12-04 19:37:08 +0530

    Add Saved Instance State

--- app/src/main/java/com/example/devalodh/twoactivities/SecondActivity.java
+++ app/src/main/java/com/example/devalodh/twoactivities/SecondActivity.java
@@ -3,6 +3,7 @@ package com.example.devalodh.twoactivities;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -13,6 +14,44 @@ public class SecondActivity extends AppCompatActivity {
             "com.example.devalodh.twoactivities.extra.REPLY";
     private EditText mReply;
 
+    private static final String LOG_TAG = SecondActivity.class.getSimpleName();
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(LOG_TAG, "onStop");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(LOG_TAG, "onPause");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(LOG_TAG, "onRestart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(LOG_TAG, "onResume");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(LOG_TAG, "onDestroy");
+    }
+
+    @Override
+    public void onStart(){
+        super.onStart();
+        Log.d(LOG_TAG, "onStart");
+    }
+
 
 
     @Override
@@ -21,7 +60,8 @@ public class SecondActivity extends AppCompatActivity {
         setContentView(R.layout.activity_second);
         mReply = findViewById(R.id.editText_second);
 
-
+        Log.d(LOG_TAG, "-------");
+        Log.d(LOG_TAG, "onCreate");
 
         Intent intent = getIntent();
         String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
@@ -36,6 +76,7 @@ public class SecondActivity extends AppCompatActivity {
         Intent replyIntent = new Intent();
         replyIntent.putExtra(EXTRA_REPLY,reply);
         setResult(RESULT_OK,replyIntent);
+        Log.d(LOG_TAG, "END Second Activity");
         finish();
     }
 }
